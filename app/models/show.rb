cdcb5270698ac630c3867f631890d78649d388ec
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-24 22:26:05 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   
   def actors_list
-    self.actors.full_name
+    self.actors.map{ |actors| actors.full_name}
   end
   
 end
