8e726fba0ea831880dc6df9d599e3f39de875f7e
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-24 22:12:31 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,9 @@
 class Actor < ActiveRecord::Base
   has_many :characters
-  has_many :shows through :characters
+  has_many :shows through: :characters
+  
+  def list_roles
+    self.shows.map(&:chraracters)
+  end
+
 end
